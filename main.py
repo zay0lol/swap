1b6edf1b1f171f2a7081f15a187800c6214541d4
zay0lol <zynahisthebest2012@gmail.com>
2024-12-28 22:39:51 +0500

    swap

---
+++ main.py
@@ -0,0 +1,13 @@
+
+a = int(input("enter first number: "))
+b = int(input("enter second number: "))
+c = int(input("enter third number: "))
+
+a=b
+b=c
+c=a
+
+print("after swap")
+print("first number:", a)
+print("second number:", b)
+print("third number:", c)
